efe6b274e65d0e836586845f580fdf1b481f0d2a
Al Ganiev <helios.ag@gmail.com>
2023-08-20 13:40:26 +1000

    Added devcontainer

---
+++ shell/.welcome.sh
@@ -0,0 +1,6 @@
+echo "Terminal Go tools:"
+echo " * mockgen: generate mocks and stubs"
+echo " * mockery: generate testify/mock mocks from interfaces"
+echo "Terminal Docker tools aliases:"
+echo " * alpine: launch an interactive alpine 3.18 container"
+
